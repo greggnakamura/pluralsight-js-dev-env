3c6db067002eea56ae1aef4435b7a9d884cd973a
Gregg Nakamura <gregg.nakamura@gmail.com>
2016-12-28 00:08:28 -0500

    Add index.html h1 test

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,27 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
-import fs from 'fs';
+import fs from 'fs';  // interact with file system using node
 
 describe('Our first test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should say hello', (done) => {
+    // cache contents of homepage
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+
+    // use jsdom
+    jsdom.env(index, function (err, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal('Hello World!');
+
+      done(); // tell mocha test is done
+
+      // free up memory
+      window.close();
+    });
   });
 });
