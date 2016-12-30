ed98908f34d580d55154b8b62de45fa2825da742
Gregg Nakamura <gregg.nakamura@gmail.com>
2016-12-29 20:54:51 -0800

    Add Users template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import './index.css';
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format('$0, 0.00');
+// populate table of users via API call
+getUsers().then(result => {
+  let usersBody = '';
 
-debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`);
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.Id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
